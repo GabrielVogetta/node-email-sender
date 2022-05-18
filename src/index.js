527a9914b8e642292cbfe31c122d6689daf701e5
Gabriel Vogetta <vogetta2002@gmail.com>
2022-05-18 17:02:39 -0300

    access control allow origin

--- src/index.js
+++ src/index.js
@@ -9,9 +9,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use(function(req, res, next){
-//     res.header('')
-// })
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
 
 app.post('/', async (req, res) => {
 
