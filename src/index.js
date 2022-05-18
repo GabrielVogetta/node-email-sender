7e8c86c90e27ebe52ddb4b6399bb85a7a8323541
Gabriel Vogetta <vogetta2002@gmail.com>
2022-05-18 18:03:27 -0300

    cors origin localhost:3000

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ const port = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.post('/', async (req, res) => {
 
